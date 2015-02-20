05b3132df1098f3d5eb29c6c7eaed9d3a1ce8281
nland <nland@authen2cate.com>
2015-02-20 16:51:30 -0500

    Init commit.
    This is working. We're able to upload the file, and we're using Spring Boot!

---
+++ spring_upload_file/src/main/java/hello/Application.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by nland on 2/20/2015.
+ */
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
